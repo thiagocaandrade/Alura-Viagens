9b2626baeceda3a7d32de4280796b592bd7f464e
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-22 18:21:29 -0300

    Initial commit

---
+++ app/src/main/java/com/thiago/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.thiago.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thiago.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
