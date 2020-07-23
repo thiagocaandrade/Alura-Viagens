286384a37b5a95ca5fc3664a23ac1139a722fd14
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-23 18:59:09 -0300

    adapter

--- app/src/main/java/com/thiago/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/thiago/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package com.thiago.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.thiago.aluraviagens.R;
+import com.thiago.aluraviagens.dao.PacoteDAO;
+import com.thiago.aluraviagens.model.Pacote;
+import com.thiago.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,12 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+
     }
 }
