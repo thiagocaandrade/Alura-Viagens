a2da330ca128c6c12978410da429c720537f025f
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-24 11:05:06 -0300

    refatoração

---
+++ app/src/main/java/com/thiago/aluraviagens/util/DiasUtil.java
@@ -0,0 +1,14 @@
+package com.thiago.aluraviagens.util;
+
+public class DiasUtil {
+
+    public static final String PLURAL = " dias";
+    public static final String SINGULAR = " dia";
+
+    public static String formataEmTexto(int quantidadeDeDias) {
+        if (quantidadeDeDias > 1){
+            return quantidadeDeDias + PLURAL;
+        }
+            return quantidadeDeDias + SINGULAR;
+    }
+}
