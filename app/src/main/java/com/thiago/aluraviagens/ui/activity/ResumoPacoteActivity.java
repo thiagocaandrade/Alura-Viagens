d5c43454d59cc7bd7798eb3219f82024ad4be8e9
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-24 11:57:56 -0300

    layout activity resumo

---
+++ app/src/main/java/com/thiago/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,14 @@
+package com.thiago.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
