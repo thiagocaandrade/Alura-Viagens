b31c448df56ffcdb1d52c2b878b71ae1fd750de9
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-26 14:41:45 -0300

    data util e button pagamento

---
+++ app/src/main/java/com/thiago/aluraviagens/util/DataUtil.java
@@ -0,0 +1,21 @@
+package com.thiago.aluraviagens.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DataUtil {
+
+    public static final String DIA_E_MES = "dd/MM";
+
+    public static String periodoEmTexto(int dias) {
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, dias);
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat(DIA_E_MES);
+        String dataFormatadaIda = formatoBrasileiro.format(dataIda.getTime());
+        String dataFormatadaVolta = formatoBrasileiro.format(dataVolta.getTime());
+        return dataFormatadaIda + " - "
+                + dataFormatadaVolta + " de "
+                + dataVolta.get(Calendar.YEAR);
+    }
+}
